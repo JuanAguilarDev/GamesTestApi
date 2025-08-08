43beef8746a94344dc77eb73172a933e8ddeb0f7
Juan Aguilar <Juan_aguilar1290@outlook.es>
2025-08-08 01:47:03 -0600

    Se agrega generico de Card

--- GameTestApi/Views/Generics/Card/CardView.swift
+++ GameTestApi/Views/Generics/Card/CardView.swift
@@ -9,42 +9,79 @@ import SwiftUI
 
 struct CardView: View {
     let game: GameModel
+    let onAddToCart: () -> Void
+    let onToggleFavorite: () -> Void
     
     var body: some View {
-        Text("Hello, World!")
-    }
-}
-
-@available(iOS 17.0, *)
-#Preview(traits: .sizeThatFitsLayout) {
-    GameCard(game: .dummyGame)
-}
-
-struct GameCard: View {
-    let game: GameModel
-    var body: some View {
-        VStack(alignment: .leading) {
-            AsyncImage(url: URL(string: game.thumbnail)) { image in
-                image.resizable().scaledToFill()
-            } placeholder: {
-                Color.gray.opacity(0.3)
+        VStack(alignment: .leading, spacing: 16) {
+            ZStack(alignment: .topTrailing) {
+                AsyncImage(url: URL(string: game.thumbnail)) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                } placeholder: {
+                    Color.gray.opacity(0.3)
+                }
+                .frame(height: 95)
+                .clipped()
+                
+                Button(action: onToggleFavorite) {
+                    Image(systemName: "heart")
+                        .padding(10)
+                        .foregroundColor(.gray)
+                        .background(Color.white.opacity(0.8))
+                        .clipShape(Circle())
+                        .padding()
+                }
             }
-            .frame(height: 120)
-            .clipped()
             
-            Text(game.title).font(.headline)
-            Text("\(game.genre) · \(game.platform) · \( game.releaseDate)")
-                .font(.subheadline).foregroundStyle(.secondary)
-            Text(game.shortDescription)
-                .font(.caption).lineLimit(1)
-            
-            Link(destination: URL(string: game.gameUrl)!) {
-                Image(systemName: "arrow.up.right")
+            VStack(alignment: .leading, spacing: 8) {
+                Text(game.title)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .frame(height: 30)
+                
+                HStack {
+                    Label(game.genre, systemImage: "gamecontroller")
+                        .padding(6)
+                        .background(Color.gray.opacity(0.1))
+                        .cornerRadius(4)
+                }
+                
+                Text(game.shortDescription)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .lineLimit(3)
+                
+                HStack {
+                    Button(action: onAddToCart) {
+                        Text("Ver Mas")
+                            .frame(maxWidth: .infinity)
+                            .font(.subheadline)
+                            .padding(.horizontal)
+                            .padding(.vertical, 8)
+                            .background(Color.purple)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
             }
-            .padding(.top, 4)
+            .padding(.horizontal)
+            .padding(.bottom)
         }
-        .padding()
-        .background(.regularMaterial)
-        .cornerRadius(8)
+        .frame(maxWidth: .infinity)
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .shadow(radius: 8)
     }
+}
+
+#Preview {
+    CardView(game: .dummyGame) {
+        print("Vista detalle")
+    } onToggleFavorite: {
+        print("Agregado a favoritos")
+    }
+    
 }
